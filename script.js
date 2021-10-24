937ff8f66066915dc5aefb266ccab5e8d88700a0
Neervel <klihopoy@mail.ru>
2021-10-24 21:07:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#works',
+    data: {
+        works: [{
+                name: 'Промо-сайт для курсов по похудению',
+                link: 'https://sergeymetalnikov.com/'
+            },
+            {
+                name: 'Delivery Food - сервис доставки',
+                link: 'https://neervel.github.io/delivery_food/'
+            },
+            {
+                name: 'Сайт ремонтной компании',
+                link: 'https://neervel.github.io/web-work/'
+            },
+            {
+                name: 'Dango - сервис совместных поездок',
+                link: 'https://neervel.github.io/dango/'
+            }
+        ]
+    }
+})
