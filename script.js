cf7f7171dbd4d31de9498d2749149a5a2708a8b1
Neervel <klihopoy@mail.ru>
2021-10-24 22:13:56 +0300

    add works

--- script.js
+++ script.js
@@ -3,19 +3,43 @@ const app = new Vue({
     data: {
         works: [{
                 name: 'Промо-сайт для курсов по похудению',
-                link: 'https://sergeymetalnikov.com/'
-            },
-            {
-                name: 'Delivery Food - сервис доставки',
-                link: 'https://neervel.github.io/delivery_food/'
-            },
-            {
-                name: 'Сайт ремонтной компании',
-                link: 'https://neervel.github.io/web-work/'
+                link: 'https://sergeymetalnikov.com/',
+                image: 'sergeymetalnikov'
             },
             {
                 name: 'Dango - сервис совместных поездок',
-                link: 'https://neervel.github.io/dango/'
+                link: 'https://neervel.github.io/dango/',
+                image: 'dango'
+            },
+            {
+                name: 'Naidenova - личный сайт художницы',
+                link: 'https://neervel.github.io/naidenova/',
+                image: 'naidenova'
+            },
+            {
+                name: 'Delivery Food - сервис доставки',
+                link: 'https://neervel.github.io/delivery_food/',
+                image: 'delivery_food'
+            },
+            {
+                name: 'Сайт ремонтной компании',
+                link: 'https://neervel.github.io/web-work/',
+                image: 'web-work'
+            },
+            {
+                name: 'Strawberry Shop - магазин фруктовых букетов',
+                link: 'https://strawberry-shop.ru/',
+                image: 'strawberry-shop'
+            },
+            {
+                name: 'Trip & MInd - турагенство',
+                link: 'https://neervel.github.io/tripMind/',
+                image: 'tripMind'
+            },
+            {
+                name: 'IndiCorp - юридическая компания',
+                link: 'https://neervel.github.io/indicorp/',
+                image: 'indicorp'
             }
         ]
     }
